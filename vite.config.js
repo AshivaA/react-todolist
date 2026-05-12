62f0802ed4c693838f1c2f46c786085a0c8af243
AshivaA <shiva.khandel86@gmail.com>
2026-05-12 16:55:32 +0100

    initialize repo

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),'prettier', 'eslint-plugin-prettier'],
+  server: {
+    open: true, // Opens the app in the browser automatically
+    port: 3000 // Sets the port number for the development server
+  }
+})
