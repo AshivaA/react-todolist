e71faeb919ca3720168263196641ba98e247e1b7
AshivaA <shiva.khandel86@gmail.com>
2026-05-19 14:48:28 +0100

    add input test and input component

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+// import React from 'react'
+import React, { useState } from 'react'
 
 function NoteInput() {
 
+const [note, setNote]= useState('');
+
+const NoteChange = (event) => setNote(event.target.value);
 
 
-    
   return (
     <div>
         
-<h1>NoteInput</h1>
+<input 
+ value={note}
+  onChange={NoteChange}
+  placeholder=' write your note here... '
+/>
+
 
     </div>
   )
--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,11 +1,38 @@
-import { render } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
-import { it } from 'vitest';
+import { expect, it } from 'vitest';
 
 
-// Test 1: Write a test to check if `App` component renders.
-it('App Component Renders Without Error', () => {
+// Test 1: Write a test to check if (App.jsx) component renders.
+it('should render App Component', () => {
     render(<App />);
 });
+
+
+// Test 2: check if input is writable.
+it('should check that user can write in input', async () => {
+   
+    // create fake user inside the test
+    const user = userEvent.setup();
+    render(<App />);
+
+    // first find the input 
+    const input = screen.getByRole('textbox');
+    console.log(input.value);
+
+     // then check if the input is writable or not.
+     //(user.type means (where you type, what you type) 
+    //  important: you are Not typing in the browser'buy milk' this line type it automatically because of the fake user.
+    await user.type(input, 'buy milk');
+    
+
+     // finally check result(I expect the input’s value to equal 'buy milk' inside the test.)
+     // (toBe() checks if it is exactly equal with, what you type in user.type.)
+    expect(input.value).toBe('buy milk');
+    
+});
+
+
+
 
